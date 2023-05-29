e637cd275a12c66a893c8fee997b2874b4ca8d30
Aleksandar Lazrag <alex.lazrag@gmail.com>
2023-05-29 15:48:17 +0300

    next commit

--- src/main/java/com/example/advanceacademy/entity/User.java
+++ src/main/java/com/example/advanceacademy/entity/User.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 import java.util.List;
 @Data
 @Builder
-@Entity
+@Entity(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
